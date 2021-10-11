c117ded90a6bd4221ba7563aa4a9557df611dc5c
Yurii <y.nadilnyi@gmail.com>
2021-10-11 16:52:30 +0300

    routing adde

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntryComponent } from './pages/entry/entry.component';
+import { MessengerComponent } from './pages/messenger/messenger.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', redirectTo: 'entry', pathMatch: 'full' },
+	{ path: 'entry', component: EntryComponent },
+	{ path: 'messenger', component: MessengerComponent },
+	{ path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
