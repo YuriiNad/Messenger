bf9c462a241c7c364061dfa41c420a3f0360b2a1
Yurii <y.nadilnyi@gmail.com>
2021-11-16 17:03:44 +0200

    routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { EntryComponent } from './pages/entry/entry.component';
 import { MessengerComponent } from './pages/messenger/messenger.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AuthGuard } from './service/auth.guard';
 
 const routes: Routes = [
-	{ path: '', redirectTo: 'entry', pathMatch: 'full' },
+	{ path: '', redirectTo: 'chat', pathMatch: 'full' },
 	{ path: 'entry', component: EntryComponent },
-	{ path: 'messenger', component: MessengerComponent },
+	{ path: 'chat', component: MessengerComponent, canActivate: [AuthGuard] },
 	{ path: '**', component: NotFoundComponent },
 ];
 
