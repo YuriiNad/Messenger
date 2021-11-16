c3c1076226fcea9be3ce1347c471840cedb507f0
Yurii <y.nadilnyi@gmail.com>
2021-11-16 17:06:52 +0200

    static users' response was added

--- src/app/service/chuck-norris-resp.service.ts
+++ src/app/service/chuck-norris-resp.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Message } from '../models/message.model';
+import { ChatService } from './chat.service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ChuckNorrisRespService {
 
-  constructor() { }
+	private url = 'https://api.chucknorris.io/jokes/random';
+
+	constructor(
+		private _http: HttpClient,
+	) {
+	}
+	staticUserResponse(): Observable<string> {
+		return this._http.get<string>(this.url)
+			.pipe(
+				map((resp: any): string => {
+					return resp.value
+				})
+			)
+	}
+
 }
