0bbe2529baeadd15e1e899904dd4587215e23f8f
Yurii <y.nadilnyi@gmail.com>
2021-11-16 16:55:42 +0200

    404 was added with stylies

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+	selector: 'app-not-found',
+	templateUrl: './not-found.component.html',
+	styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent implements OnInit {
+	constructor(
+		public _afAuth: AngularFireAuth,
+		private router: Router,
+	) {
+	}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+	goBackTo(goTo: string): void {
+		this.router.navigate([`/${goTo}`])
+	}
 
 }
