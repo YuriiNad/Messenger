e0f67c9fbce40539fab279920d79acad837bd007
Yurii <y.nadilnyi@gmail.com>
2021-11-16 17:01:29 +0200

    static users were added with styles

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable } from 'rxjs';
+import { ChatUser } from 'src/app/models/chatUser.model';
+import { Message } from 'src/app/models/message.model';
+import { ChatService } from 'src/app/service/chat.service';
+import { ShareService } from 'src/app/service/share.service';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+	selector: 'app-user',
+	templateUrl: './user.component.html',
+	styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+	public lastMessage!: Message;
+	@Input() staticUsers!: ChatUser[];
 
-  constructor() { }
+	constructor(
+		private _share: ShareService,
+		private _chat: ChatService,
+		private _db: AngularFireDatabase,
+	) {
 
-  ngOnInit(): void {
-  }
+	}
+
+	ngOnInit(): void {
+	}
+
+	selectChat(person: any, img: any) {
+		this._share.getSendToUser(person, img);
+	}
+
+
 
 }
+
