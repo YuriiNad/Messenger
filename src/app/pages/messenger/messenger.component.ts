f33b514761a2dbdb61a743e189e57fa2804367f0
Yurii <y.nadilnyi@gmail.com>
2021-11-16 17:06:04 +0200

    messenger adjusted accordingly

--- src/app/pages/messenger/messenger.component.ts
+++ src/app/pages/messenger/messenger.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterContentChecked, AfterViewChecked, Component, DoCheck, OnChanges, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/service/auth.service';
+import { ChatService } from 'src/app/service/chat.service';
 @Component({
-  selector: 'app-messenger',
-  templateUrl: './messenger.component.html',
-  styleUrls: ['./messenger.component.scss']
+	selector: 'app-messenger',
+	templateUrl: './messenger.component.html',
+	styleUrls: ['./messenger.component.scss']
 })
-export class MessengerComponent implements OnInit {
+export class MessengerComponent implements OnInit, AfterContentChecked {
+	public displayName!: Observable<string>;
 
-  constructor() { }
+	constructor(
+		public _auth: AuthService,
+		private _chat: ChatService,
+	) {
 
-  ngOnInit(): void {
-  }
+	}
 
+	signOut() {
+		this._auth.signOut();
+	}
+	ngOnInit(): void {
+		// this.displayName = 'TestUser'
+	}
+	ngAfterContentChecked() {
+		this.displayName = this._chat.displayName
+	}
 }
