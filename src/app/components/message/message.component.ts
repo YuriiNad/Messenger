a6b002afd45cbf912bc608bf103e630a08051fd2
Yurii <y.nadilnyi@gmail.com>
2021-11-16 16:57:23 +0200

    message component  adjusted

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ShareService } from 'src/app/service/share.service';
 
 @Component({
-  selector: 'app-message',
-  templateUrl: './message.component.html',
-  styleUrls: ['./message.component.scss']
+	selector: 'app-message',
+	templateUrl: './message.component.html',
+	styleUrls: ['./message.component.scss']
 })
 export class MessageComponent implements OnInit {
+	public staticUserImg!: string;
 
-  constructor() { }
+	@Input() allMessages!: Observable<any>;
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private _share: ShareService,
+	) { }
 
+	ngOnInit(): void {
+		this.setStaticUser();
+	}
+
+	setStaticUser(): void {
+		// sets staticUserImg
+		this._share.sendUserImg.subscribe((img: string) => {
+			this.staticUserImg = img;
+		})
+	}
 }
