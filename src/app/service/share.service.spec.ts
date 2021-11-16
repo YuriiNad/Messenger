1511043ff07a8ad90ebdb0d45aae1873844cb513
Yurii <y.nadilnyi@gmail.com>
2021-11-16 17:07:53 +0200

    passing of static users' data was addedd

--- src/app/service/share.service.spec.ts
+++ src/app/service/share.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { ShareService } from './share.service';
 
 describe('ShareService', () => {
-  let service: ShareService;
+	let service: ShareService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ShareService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({});
+		service = TestBed.inject(ShareService);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
 });
--- src/app/service/share.service.ts
+++ src/app/service/share.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { BehaviorSubject } from 'rxjs';
+import { ChatListComponent } from '../components/chat-list/chat-list.component';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ShareService {
+	private staticUserName = new BehaviorSubject<string>('Rachel Green-Geller');
+	private staticUserImg = new BehaviorSubject<string>('../../../assets/img/Rachel\ Green-Geller.jpg');
 
-  constructor() { }
+	sendUserName = this.staticUserName.asObservable();
+	sendUserImg = this.staticUserImg.asObservable();
+
+
+
+	constructor(
+		private _db: AngularFireDatabase,
+	) {
+
+	}
+
+
+	getSendToUser(sendUsers: string, img: string) {
+		this.staticUserName.next(sendUsers)
+		this.staticUserImg.next(img)
+	}
+
+
 }
